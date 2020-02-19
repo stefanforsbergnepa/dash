4f00f43b8066aa0f65c1fb56ca104a6c881bb6bb
Stefan Forsberg <stefan.forsberg@nepa.com>
2020-02-19 13:07:20 +0100

    Lessons04

---
+++ lesson04/lesson04.tests/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+using System.Collections.Generic;
+
+namespace lesson04.tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void NumberedRows1()
+        {
+            var pyramid = new Pyramid();
+            var result = pyramid.NumberedRows1(5);
+
+            var expected = 
+@"
+1
+22
+333
+4444
+55555";
+
+            result.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void NumberedRows2()
+        {
+            var pyramid = new Pyramid();
+            var result = pyramid.NumberedRows2(5);
+
+            var expected = 
+@"
+1
+23
+456
+78910
+1112131415";
+
+            result.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void Flipped()
+        {
+            var pyramid = new Pyramid();
+            var result = pyramid.Flipped(5);
+
+            var expected = 
+@"
+    1
+   22
+  333
+ 4444
+55555";
+
+            result.ShouldBe(expected);
+        }
+    }
+}
---
+++ lesson04/lesson04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lesson04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Pyramid
+    {
+        public string NumberedRows1(int rows)
+        {
+
+        }
+
+        public string NumberedRows2(int rows)
+        {
+            return "";
+        }
+
+        public string Flipped(int rows)
+        {
+            return "";
+        }
+    }
+
+  
+}
+ 
+
+
+
