2f81c7c8139187357177c9e36e2bf59b68a551d2
Niklas Melinder <niklas.melinder@nepa.com>
2020-02-24 14:33:46 +0100

    Lesson 5 - Basic sampler tests

---
+++ lesson05/lesson05.tests/SamplerTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace lesson05.tests
+{
+    public class SamplerTests
+    {
+        [Fact]
+        public void Sample_with_size()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com" },
+                new Unit { Id = "unit2@email.com" },
+                new Unit { Id = "unit3@email.com" },
+                new Unit { Id = "unit4@email.com" }
+            };
+
+            var sampler = new Sampler();
+            var sample = sampler.CreateSampleWithSize(population, 3);
+
+            // Sample should contain 3 units
+            sample.Length.ShouldBe(3);
+        }
+
+        [Fact]
+        public void Ordering_sample_decending()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com" },
+                new Unit { Id = "unit2@email.com" },
+                new Unit { Id = "unit3@email.com" }
+            };
+
+            var sampler = new Sampler();
+            var sample = sampler.CreateSampleWithSize(population, 3);
+
+            // Sample should be sorted in descending order on id
+            sample.Length.ShouldBe(3);
+            sample[0].Id.ShouldBe("unit3@email.com");
+            sample[1].Id.ShouldBe("unit2@email.com");
+            sample[2].Id.ShouldBe("unit1@email.com");
+        }
+
+        [Fact]
+        public void Get_units_for_targetgroup()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com", TargetGroup = "TG1" },
+                new Unit { Id = "unit2@email.com", TargetGroup = "TG2" },
+                new Unit { Id = "unit3@email.com", TargetGroup = "TG1" },
+                new Unit { Id = "unit4@email.com", TargetGroup = "TG3" }
+            };
+
+            var sampler = new Sampler();
+            var sample = sampler.GetUnitsForTargetGroup(population, "TG1");
+
+            // Sample should only contain units for targetgroup TG1
+            sample.Length.ShouldBe(2);
+            sample.ShouldAllBe(x => x.TargetGroup == "TG1");
+        }
+
+        [Fact]
+        public void Get_sampleable_units()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com", VisitDate = DateTime.Today.AddDays(-1) },
+                new Unit { Id = "unit2@email.com", VisitDate = DateTime.Today.AddDays(-1) },
+                new Unit { Id = "unit3@email.com", VisitDate = DateTime.Today.AddDays(-5) },
+                new Unit { Id = "unit4@email.com", VisitDate = DateTime.Today.AddDays(-5) }
+            };
+
+            var sampler = new Sampler();
+            var sample = sampler.GetSampleableUnits(population, DateTime.Today.AddDays(-2));
+
+            // Sample should only contain units that are at most 2 days old
+            sample.Length.ShouldBe(2);
+            sample.ShouldAllBe(x => x.VisitDate == DateTime.Today.AddDays(-1));
+        }
+
+        [Fact]
+        public void Remove_quarantined_units()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com", VisitDate = DateTime.Now.AddDays(-1) },
+                new Unit { Id = "unit2@email.com", VisitDate = DateTime.Now.AddDays(-1) },
+                new Unit { Id = "unit3@email.com", VisitDate = DateTime.Now.AddDays(-1) }
+            };
+
+            var quarantine = new [] { "unit1@email.com", "unit2@email.com" };
+
+            var sampler = new Sampler();
+            var sample = sampler.RemoveQuarantines(population, quarantine);
+
+            // Sample should not contain units in quarantine
+            sample.Length.ShouldBe(1);
+            sample[0].Id.ShouldBe("unit3@email.com");
+        }
+
+        [Fact]
+        public void Sample_on_targetgroups()
+        {
+            var population = new []
+            {
+                new Unit { Id = "unit1@email.com", TargetGroup = "TG1" },
+                new Unit { Id = "unit2@email.com", TargetGroup = "TG1" },
+                new Unit { Id = "unit3@email.com", TargetGroup = "TG1" },
+                new Unit { Id = "unit4@email.com", TargetGroup = "TG2" },
+                new Unit { Id = "unit5@email.com", TargetGroup = "TG2" },
+                new Unit { Id = "unit6@email.com", TargetGroup = "TG2" }
+            };
+
+            var targetGroups = new []
+            {
+                new TargetGroup { Id = "TG1", SampleSize = 2 },
+                new TargetGroup { Id = "TG2", SampleSize = 2 }
+            };
+
+            var sampler = new Sampler();
+            var sample = sampler.CreateSampleForTargetGroups(population, targetGroups);
+
+            // TG1 should have 2 units sampled
+            // TG2 should have 2 units sampled
+            sample.Length.ShouldBe(4);
+            sample.Count(x => x.TargetGroup == "TG1").ShouldBe(2);
+            sample.Count(x => x.TargetGroup == "TG2").ShouldBe(2);
+        }
+    }
+}
---
+++ lesson05/lesson05/Sampler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lesson05
+{
+    public class Sampler
+    {
+        public Unit[] CreateSampleWithSize(Unit[] population, int sampleSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit[] GetSampleableUnits(Unit[] population, DateTime sampleFrom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit[] RemoveQuarantines(Unit[] population, string[] quarantine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit[] GetUnitsForTargetGroup(Unit[] population, string targetGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit[] CreateSampleForTargetGroups(Unit[] population, TargetGroup[] targetGroups)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Unit
+    {
+        public string Id { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string TargetGroup { get; set; }
+    }
+
+    public class TargetGroup
+    {
+        public string Id { get; set; }
+        public int SampleSize { get; set; }
+    }
+}
